0e4b81268ba3b5dc89edd41dfbf827ddf307a509
olhasemenova <olha.sergeevna87@gmail.com>
2022-06-14 13:44:04 +0300

    add 2** task

--- hw1_additional task.js
+++ hw1_additional task.js
@@ -2,17 +2,22 @@ let age_1 = 17;
 let age_2 = 18;
 let age_3 = 61;
 
-
+//Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
 const checkAge = function(age) {
-    if (age < age_1){
-        console.log(`You don’t have access cause your age is ${age}. It’s less then`);
-    } else if(age >= age_2 && age < age_3){
-        console.log('“Welcome  !”');
-    } else if(age > age_3){
-        console.log('Keep calm and look Culture channel.');
-    } else{
-        console.log('Technical work');   
+    if (typeof(age) === 'number'){
+        if (age < age_1){
+            console.log(`You don’t have access cause your age is ${age}. It’s less then`);
+        } else if(age >= age_2 && age < age_3){
+            console.log('“Welcome  !”');
+        } else if(age > age_3){
+            console.log('Keep calm and look Culture channel.');
+        } else{
+            console.log('Technical work');
+        }
+    }
+    else{
+        console.log('Enter number value')
     }
 }
 
-checkAge(6);
+checkAge('st');
